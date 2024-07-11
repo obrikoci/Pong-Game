8e72810fb377864a8a917300f07da298f2f7021e
Obri Koci <obrikoci2@gmail.com>
2024-07-11 19:56:16 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,51 @@
+from turtle import Turtle, Screen
+from paddle import Paddle
+from ball import Ball
+from scoreboard import ScoreBoard
+import time
+
+screen = Screen()
+screen.setup(width=800, height=600)
+screen.bgcolor("black")
+screen.title("Pong")
+screen.tracer(0)
+
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-355, 0))
+ball = Ball()
+scoreboard = ScoreBoard()
+
+
+screen.listen()
+screen.onkey(key="Up", fun=r_paddle.go_up)
+screen.onkey(key="Down", fun=r_paddle.go_down)
+screen.onkey(key="w", fun=l_paddle.go_up)
+screen.onkey(key="s", fun=l_paddle.go_down)
+
+
+game_on = True
+while game_on:
+    time.sleep(ball.move_speed)
+    screen.update()
+    ball.move()
+
+    # Detect collision with wall
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
+
+    # Detect collision with paddle
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+        ball.bounce_x()
+
+    # When r_paddle misses ball
+    if ball.xcor() > 380:
+        ball.reset_pos()
+        scoreboard.l_point()
+
+    # When l_paddle misses ball
+    if ball.xcor() < -380:
+        ball.reset_pos()
+        scoreboard.r_point()
+
+
+screen.exitonclick()
